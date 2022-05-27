9f50e171b4be8d488cbb4118ffa10214947b7367
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-05-27 14:35:40 -0300

    feat: implementando estilos na pagina home

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
@@ -22,9 +23,20 @@ export async function getStaticProps() {
 
 export default function Home({ pokemons }) {
   return (
-    <div>
-      <h1>PokeNext</h1>
-      <ul>
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image
+          src="/images/pokeball.png"
+          width="50"
+          height="50"
+          alt="Pokeball"
+        />
+      </div>
+
+      <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
           <li key={pokemon.id}>
             <Link href="/pokemon/[id]">
@@ -32,7 +44,7 @@ export default function Home({ pokemons }) {
             </Link>
           </li>
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   );
 }
