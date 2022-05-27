09decd30b1272017c7fbb67936e6fc95fe06fa09
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-05-27 14:24:44 -0300

    feat: implementacao da chamada da api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = 'https://pokeapi.co/api/v2/pokemon/';
+
+  const response = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await response.json();
+
+  // add pokemon index
+  data.results.forEach((pokemon, index) => {
+    pokemon.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
   return (
-    <h1>PokeNext</h1>
-  )
+    <div>
+      <h1>PokeNext</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            <Link href="/pokemon/[id]">
+              <a>{pokemon.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
